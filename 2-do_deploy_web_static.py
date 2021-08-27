25a394a80bff55cc32495c72f517c51008c58e55
Daniel Millan <danielmillanalvarez@gmail.com>
2021-08-27 00:52:05 +0000

    fixing importts 2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,15 +1,13 @@
 #!/usr/bin/python3
 ''' Fabric script that generates a .tgz archive '''
 
-from fabric.api import local, env, put, run
+from fabric.api import *
 from datetime import datetime
-from os import path
 
 env.hosts = [
     '35.231.143.58',
     '35.185.49.2'
 ]
-
 env.user = "ubuntu"
 
 
@@ -26,11 +24,11 @@ def do_pack():
 
 def do_deploy(archive_path):
     ''' distributes an archive to my web servers '''
-    if (not path.exists(archive_path)):
+    if (not archive_path):
         return (False)
 
     try:
-        put(archive_path, '/tmp/')
+        put(archive_path, '/tmp')
         folder_path = "/data/web_static/releases/"+archive_path[9:-4]+"/"
         run('mkdir -p '+folder_path)
         run('tar -zxvf /tmp/'+archive_path[9:]+' -C '+folder_path)
