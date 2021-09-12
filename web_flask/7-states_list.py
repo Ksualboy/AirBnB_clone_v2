caf47de8cbe66eb74b2c6d75a4e03793b2397cc3
Ksualboy <2758@holbertonschool.com>
2021-09-12 23:25:32 +0000

    fixing imports

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -2,11 +2,12 @@
 ''' starts a Flask web application '''
 
 from flask import Flask, render_template
+from models.state import State
 from models import storage
 app = Flask(__name__)
 
 
-@app.route("/states_list", strict_slashes=False)
+@app.route('/states_list', strict_slashes=False)
 def xd():
     ''' displays all the states in the html '''
     return render_template('7-states_list.html',
@@ -20,4 +21,4 @@ def mr_closer(self):
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0')
