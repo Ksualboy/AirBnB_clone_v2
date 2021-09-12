a6fee50a875d269fffca79c729b79e3bd78d03a6
Ksualboy <2758@holbertonschool.com>
2021-09-12 23:00:47 +0000

    adding 8

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+''' starts a Flask web application '''
+
+from flask import Flask, render_template
+from models import storage
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def mr_closer(self):
+    ''' removes the current SQLAlchemy Session '''
+    storage.close()
+
+
+@app.route("/states_list", strict_slashes=False)
+def xd():
+    ''' displays all the states in the html '''
+    return render_template('7-states_list.html',
+                           states=storage.all('State').values())
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
