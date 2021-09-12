bbf01ea8f8594f86add21fac62f1c643d916fd39
Ksualboy <2758@holbertonschool.com>
2021-09-12 21:25:29 +0000

    adding 4

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+''' starts a Flask web application '''
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def root():
+    ''' puts something '''
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    ''' same but for hbnb'''
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c_is_fun(text):
+    '''same but with a variable'''
+    new_text = text.replace("_", " ")
+    return ("C " + new_text)
+
+
+@app.route("/python", strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def python_is_cool(text="is cool"):
+    ''' same but with python '''
+    new_text = text.replace("_", " ")
+    return ("Python " + new_text)
+
+
+@app.route("/number/<int:n>", strict_slashes=False)
+def int_check(n):
+    return ("{} is a number".format(n))
+    
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
