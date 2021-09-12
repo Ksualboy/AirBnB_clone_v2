11ff18e2bbad49fc459bd3077f5632f1fc8a98c7
Ksualboy <2758@holbertonschool.com>
2021-09-12 23:17:55 +0000

    fixing tabulations html

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -3,14 +3,7 @@
 
 from flask import Flask, render_template
 from models import storage
-from models.state import State
 app = Flask(__name__)
-
-
-@app.teardown_appcontext
-def teardown(self):
-    ''' removes the current SQLAlchemy Session '''
-    storage.close()
 
 
 @app.route("/states_list", strict_slashes=False)
@@ -18,6 +11,13 @@ def xd():
     ''' displays all the states in the html '''
     return render_template('7-states_list.html',
                            states=storage.all('State').values())
+
+
+@app.teardown_appcontext
+def mr_closer(self):
+    ''' removes the current SQLAlchemy Session '''
+    storage.close()
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
