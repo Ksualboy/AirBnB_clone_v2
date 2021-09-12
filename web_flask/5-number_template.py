b27618b7fd60ed2cf970b725af08a76949968fea
Ksualboy <2758@holbertonschool.com>
2021-09-12 21:40:32 +0000

    adding 5

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+''' starts a Flask web application '''
+
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def root():
+    ''' puts something '''
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    ''' same but for hbnb'''
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c_is_fun(text):
+    '''same but with a variable'''
+    new_text = text.replace("_", " ")
+    return ("C " + new_text)
+
+
+@app.route("/python", strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def python_is_cool(text="is cool"):
+    ''' same but with python '''
+    new_text = text.replace("_", " ")
+    return ("Python " + new_text)
+
+
+@app.route("/number/<int:n>", strict_slashes=False)
+def int_check(n):
+    return ("{} is a number".format(n))
+
+
+@app.route("/number_template/<int:n>", strict_slashes=False)
+def template_int_check(n):
+    return render_template('5-number.html', n=n)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
