57223f6863ae98636943ec4b644e978fc63a007e
Daniel Mishan <77964799+Ksualboy@users.noreply.github.com>
2021-09-12 20:40:24 -0300

    Si esto funciona, dios existe

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,24 +1,25 @@
 #!/usr/bin/python3
-''' starts a Flask web application '''
+"""a script that starts a Flask web application"""
 
-from flask import Flask, render_template
-from models.state import State
 from models import storage
+from models.state import State
+from flask import Flask
+from flask import render_template
 app = Flask(__name__)
 
 
 @app.route('/states_list', strict_slashes=False)
 def states():
-    ''' displays all the states in the html '''
+    """Returns a html listing the states"""
     return render_template('7-states_list.html',
                            states=storage.all('State').values())
 
 
 @app.teardown_appcontext
 def teardown(self):
-    ''' removes the current SQLAlchemy Session '''
+    """remove the current SQLAlchemy Session"""
     storage.close()
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0')
