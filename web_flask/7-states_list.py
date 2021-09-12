e71bf8f42f54daf1e0417cdeaa3b8ad0e7c09474
Ksualboy <2758@holbertonschool.com>
2021-09-12 23:27:42 +0000

    fixing names and imports

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,24 +1,25 @@
 #!/usr/bin/python3
 ''' starts a Flask web application '''
 
-from flask import Flask, render_template
+from flask import Flask
+from flask import render_template
 from models.state import State
 from models import storage
 app = Flask(__name__)
 
 
 @app.route('/states_list', strict_slashes=False)
-def xd():
+def myfunction():
     ''' displays all the states in the html '''
     return render_template('7-states_list.html',
                            states=storage.all('State').values())
 
 
 @app.teardown_appcontext
-def mr_closer(self):
+def closer(self):
     ''' removes the current SQLAlchemy Session '''
     storage.close()
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', port=5000)
